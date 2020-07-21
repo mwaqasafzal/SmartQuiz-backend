b53c6f4d8d1af7b090b9508648f85eb91a88e467
mwaqasafzal <mwaqas7064@gmail.com>
2020-07-22 01:25:49 +0500

    updated db link

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ dotenv.config({
 });
 const PORT = process.env.PORT || 5000;
 
-let DB = process.env.LOCAL_DB;
+let DB = process.env.DB;
 // if (process.env.NODE_ENV === "development")
 //   DB = process.env.LOCAL_DB;
 // else
