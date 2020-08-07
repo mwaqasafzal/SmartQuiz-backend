c562cf0f5e9033215e11b5ba8e16b3676373a451
mwaqasafzal <mwaqas7064@gmail.com>
2020-08-07 16:36:32 +0500

    cookies config changed

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -17,7 +17,8 @@ const createSendToken = (user, res) => {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
     httpOnly: true,
-    sameSite: 'Strict'
+    sameSite: 'none',
+    secure
   });
 
   user.password = undefined;
