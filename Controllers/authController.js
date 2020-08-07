0eb4bdca60522059a0f094b5898a3f9daa8139b0
mwaqasafzal <mwaqas7064@gmail.com>
2020-08-07 17:08:08 +0500

    new cookies config

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -18,7 +18,7 @@ const createSendToken = (user, res) => {
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
     httpOnly: true,
     sameSite: 'none',
-    secure
+    secure: true
   });
 
   user.password = undefined;
